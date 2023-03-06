fe8cd64f64ff95f97e6f2fb8307b4bb6b238e01b
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-03-06 21:44:44 +0900

    -z

---
+++ fish2meta.py
@@ -0,0 +1,133 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+plt.style.use('ggplot')
+
+
+def exp_model(x, a, b, c):
+    return a - (b * np.exp(-x / c))
+
+file = "Fish2Meta Data - Ex 3 - 1_27_23.csv"
+with open(file) as f:
+    data = f.readlines()
+
+n_participants = data[0].count(',')+1
+#for experiment 3
+stage = ['before_u', 'before_o', 'prism_u', 'prism_o', 'after_u', 'after_o']
+#for experimetn 2
+#stage = ['before_u', 'before_o', 'prism', 'after_u', 'after_o']
+
+exp_data = [{x: [] for x in stage} for _ in range(n_participants)]
+duration = [15, 15, 40, 40, 15, 15]
+times = [1]
+for d in duration:
+    times.append(times[-1] + d)
+x_values = {x: list(range(t, t + d)) for x, t, d in zip(stage, times, duration)}
+
+for x, d in enumerate(data):
+    values = list(map(float, d.strip().split(',')))
+    for v, p in zip(values, exp_data):
+        for k, t in x_values.items():
+            if x + 1 in t:
+                p[k].append(v)
+
+subject = 4
+my_colors = {'before_u': 'b', 'before_o': 'g', 'prism_u': 'r','prism_o': 'r', 'after_u': 'b', 'after_o': 'g'}
+# my_colors = {'before_u': 'b', 'before_o': 'g', 'prism': 'r', 'after_u': 'b', 'after_o': 'g'}#exp 2
+print(exp_data[subject])
+plt.figure(figsize=(14, 9))
+
+x = 1  # start of the graph
+model = {} # decay constants
+for st in stage:
+    y_values = exp_data[subject][st]
+    x_model = []
+    for n in y_values:
+        x_model.append(x)
+        plt.plot(x, n, 'o', markeredgecolor=my_colors[st], markersize=15, fillstyle='none')
+        x += 2
+
+    x += 5 #space between stages
+    plt.vlines(x, -100, 100, colors='gray')
+    x += 5  # space between stages
+    if st in ['prism_u','prism_o','after_u', 'after_o']:
+        x_norm = (np.array(x_model) - x_model[0]) / (x_model[-1] - x_model[0])  # normalized
+        a_0 = np.mean(y_values[-5:-1]) # final value exponential function moves towards
+        c_0 = 1 # the decay constant
+        b_0 = (y_values[0] - a_0) # the magnitude of adaptation
+        popt, pcov = curve_fit(exp_model, x_norm, y_values, p0=(a_0, b_0, c_0))
+        plt.plot(x_model, exp_model(x_norm, *popt), color='black',linestyle="dashed")
+
+        c_ori = (x_model[-1] - x_model[0]) * popt[2]
+        model[st] = (popt[0], popt[1], c_ori)
+        print(f"{popt[0]:.1f}{'-+'[popt[1]>0]}{abs(popt[1]):.1f}e(-t/{c_ori:.1f})",)
+#
+
+#
+#
+plt.axhline(y=0, color='gray')
+plt.ylim(-100, 100)
+plt.ylabel('HORIZONTAL DISPLACEMENT (cm)', fontsize=22)
+xlabel = '$TIME \longrightarrow$\n'
+models =['prism_u','prism_o','after_u', 'after_o']
+# models =['prism','after_u', 'after_o'] Exp 2
+for s in models:
+    m = model[s]
+    xlabel+=f"${m[0]:.1f}{'-+'[m[1]>0]}{abs(m[1]):.1f}e(-t/{m[2]:.1f})$  "
+
+plt.xlabel(xlabel, fontsize=22)
+plt.yticks(fontsize=20)
+plt.xticks([])
+plt.text(-5, -90, '$BEFORE_u$', fontsize=20)
+plt.text(37, -90, '$BEFORE_o$', fontsize=20)
+plt.text(95, -90, '$PRISMS_u$', fontsize=20)
+plt.text(185, -90, '$PRISMS_o$', fontsize=20)
+plt.text(260, -90, '$AFTER_u$', fontsize=20)
+plt.text(300, -90, '$AFTER_o$', fontsize=20)
+plt.title(f'SUBJECT {subject + 1} {file}', fontsize=22)
+
+# PC standard deviation of the 8 throws before googles
+PC_1 = np.std(exp_data[subject][stage[0]])
+PC_2 = np.std(exp_data[subject][stage[1]])
+plt.text(-5, 90, f'$PC = {PC_1:.1f}$', fontsize=20)
+plt.text(37, 90, f'$PC = {PC_2:.1f}$', fontsize=20)
+plt.text(95, 90, f'$AC = {model[stage[2]][2]:.1f}$', fontsize=20)
+plt.text(185, 90, f'$AC = {model[stage[3]][2]:.1f}$', fontsize=20)
+plt.text(260, 90, f'$AC = {model[stage[4]][2]:.1f}$', fontsize=20)
+plt.text(300, 90, f'$AC = {model[stage[5]][2]:.1f}$', fontsize=20)
+plt.savefig('media/final_plots/exp_3/5.png')
+output=f"""
+PCu = {PC_1:.1f}
+PCo = {PC_1:.1f}
+ACpu = {model[stage[2]][2]:.1f}
+ACpo = {model[stage[3]][2]:.1f}
+ACu =  {model[stage[4]][2]:.1f}
+ACo =  {model[stage[5]][2]:.1f}
+"""
+print(output)
+plt.show()
+print(model)
+
+#plt.savefig('media/final_plots/avg.png')
+
+# Exp2
+# plt.xlabel(xlabel, fontsize=22)
+# plt.yticks(fontsize=20)
+# plt.xticks([])
+# plt.text(-5, -90, '$BEFORE_u$', fontsize=20)
+# plt.text(37, -90, '$BEFORE_o$', fontsize=20)
+# plt.text(105, -90, '$PRISMS$', fontsize=20)
+# plt.text(170, -90, '$AFTER_u$', fontsize=20)
+# plt.text(210, -90, '$AFTER_o$', fontsize=20)
+# plt.title(f'SUBJECT {subject + 1}', fontsize=22)
+#
+# # PC standard deviation of the 8 throws before googles
+# PC_1 = np.std(exp_data[subject][stage[0]])
+# PC_2 = np.std(exp_data[subject][stage[1]])
+# plt.text(-5, 90, f'$PC = {PC_1:.1f}$', fontsize=20)
+# plt.text(37, 90, f'$PC = {PC_2:.1f}$', fontsize=20)
+# plt.text(110, 90, f'$AC = {model[stage[2]][2]:.1f}$', fontsize=20)
+# plt.text(170, 90, f'$AC = {model[stage[3]][2]:.1f}$', fontsize=20)
+# plt.text(210, 90, f'$AC = {model[stage[4]][2]:.1f}$', fontsize=20)
+# plt.show()
---
+++ media/final_plots/final.md
@@ -0,0 +1,41 @@
+**Subject 1**
+* PCu = 5.6
+* PCo = 5.6
+* ACpu = 5.1
+* ACpo = -18.0
+* ACu =  -38.2 
+* ACo =  7.1
+
+**Subject 2**
+PCu = 13.2
+PCo = 13.2
+ACpu = 6.4
+ACpo = 45.1
+ACu =  14.8
+ACo =  15.1
+
+
+**Subject 3**
+PCu = 6.7
+PCo = 6.7
+ACpu = 4.1
+ACpo = 3.8
+ACu =  158791.2
+ACo =  93357.1
+
+**Subject 4**
+PCu = 5.6
+PCo = 5.6
+ACpu = 2.8
+ACpo = 17.5
+ACu =  6.0
+ACo =  12.5
+
+**Subject 5**
+PCu = 8.0
+PCo = 8.0
+ACpu = 16.4
+ACpo = 255.9
+ACu =  184847.3
+ACo =  2.5
+

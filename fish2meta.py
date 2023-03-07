fdc4fe883860aa6ff91c0c80d8a209450e640d3d
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-03-07 22:23:09 +0900

    -z

--- fish2meta.py
+++ fish2meta.py
@@ -8,18 +8,20 @@ plt.style.use('ggplot')
 def exp_model(x, a, b, c):
     return a - (b * np.exp(-x / c))
 
-file = "Fish2Meta Data - Ex 3 - 1_27_23.csv"
+#file = "Fish2Meta Data - Ex 3 - 1_27_23.csv"
+file="Fish2Meta Data - Ex 2 - 11_30_22.csv"
 with open(file) as f:
     data = f.readlines()
 
 n_participants = data[0].count(',')+1
 #for experiment 3
-stage = ['before_u', 'before_o', 'prism_u', 'prism_o', 'after_u', 'after_o']
+#stage = ['before_u', 'before_o', 'prism_u', 'prism_o', 'after_u', 'after_o']
 #for experimetn 2
-#stage = ['before_u', 'before_o', 'prism', 'after_u', 'after_o']
+stage = ['before_u', 'before_o', 'prism', 'after_u', 'after_o']
 
 exp_data = [{x: [] for x in stage} for _ in range(n_participants)]
-duration = [15, 15, 40, 40, 15, 15]
+#duration = [15, 15, 40, 40, 15, 15] #exp2
+duration = [15,15,40,15,15]
 times = [1]
 for d in duration:
     times.append(times[-1] + d)
@@ -32,10 +34,10 @@ for x, d in enumerate(data):
             if x + 1 in t:
                 p[k].append(v)
 
-subject = 4
-my_colors = {'before_u': 'b', 'before_o': 'g', 'prism_u': 'r','prism_o': 'r', 'after_u': 'b', 'after_o': 'g'}
-# my_colors = {'before_u': 'b', 'before_o': 'g', 'prism': 'r', 'after_u': 'b', 'after_o': 'g'}#exp 2
-print(exp_data[subject])
+subject = 0
+#my_colors = {'before_u': 'b', 'before_o': 'g', 'prism_u': 'r','prism_o': 'r', 'after_u': 'b', 'after_o': 'g'}
+my_colors = {'before_u': 'b', 'before_o': 'g', 'prism': 'r', 'after_u': 'b', 'after_o': 'g'}#exp 2
+#print(exp_data[subject])
 plt.figure(figsize=(14, 9))
 
 x = 1  # start of the graph
@@ -51,7 +53,8 @@ for st in stage:
     x += 5 #space between stages
     plt.vlines(x, -100, 100, colors='gray')
     x += 5  # space between stages
-    if st in ['prism_u','prism_o','after_u', 'after_o']:
+    #if st in ['prism_u','prism_o','after_u', 'after_o']: #exp2
+    if st in ['prism','after_u','after_o']:
         x_norm = (np.array(x_model) - x_model[0]) / (x_model[-1] - x_model[0])  # normalized
         a_0 = np.mean(y_values[-5:-1]) # final value exponential function moves towards
         c_0 = 1 # the decay constant
@@ -70,64 +73,60 @@ plt.axhline(y=0, color='gray')
 plt.ylim(-100, 100)
 plt.ylabel('HORIZONTAL DISPLACEMENT (cm)', fontsize=22)
 xlabel = '$TIME \longrightarrow$\n'
-models =['prism_u','prism_o','after_u', 'after_o']
-# models =['prism','after_u', 'after_o'] Exp 2
+#models =['prism_u','prism_o','after_u', 'after_o']
+models =['prism','after_u', 'after_o'] #Exp 2
 for s in models:
     m = model[s]
     xlabel+=f"${m[0]:.1f}{'-+'[m[1]>0]}{abs(m[1]):.1f}e(-t/{m[2]:.1f})$  "
+#
+# plt.xlabel(xlabel, fontsize=22)
+# plt.yticks(fontsize=20)
+# plt.xticks([])
+# plt.text(-5, -90, '$BEFORE_u$', fontsize=20)
+# plt.text(37, -90, '$BEFORE_o$', fontsize=20)
+# plt.text(95, -90, '$PRISMS_u$', fontsize=20)
+# plt.text(185, -90, '$PRISMS_o$', fontsize=20)
+# plt.text(260, -90, '$AFTER_u$', fontsize=20)
+# plt.text(300, -90, '$AFTER_o$', fontsize=20)
+# plt.title(f'SUBJECT {subject + 1} {file}', fontsize=22)
 
+# PC standard deviation of the 8 throws before googles
+# PC_1 = np.std(exp_data[subject][stage[0]])
+# PC_2 = np.std(exp_data[subject][stage[1]])
+# plt.text(-5, 90, f'$PC = {PC_1:.1f}$', fontsize=20)
+# plt.text(37, 90, f'$PC = {PC_2:.1f}$', fontsize=20)
+# plt.text(95, 90, f'$AC = {model[stage[2]][2]:.1f}$', fontsize=20)
+# plt.text(185, 90, f'$AC = {model[stage[3]][2]:.1f}$', fontsize=20)
+# plt.text(260, 90, f'$AC = {model[stage[4]][2]:.1f}$', fontsize=20)
+# plt.text(300, 90, f'$AC = {model[stage[5]][2]:.1f}$', fontsize=20)
+# plt.show()
+# print(model)
+
+#Exp2
 plt.xlabel(xlabel, fontsize=22)
 plt.yticks(fontsize=20)
 plt.xticks([])
 plt.text(-5, -90, '$BEFORE_u$', fontsize=20)
 plt.text(37, -90, '$BEFORE_o$', fontsize=20)
-plt.text(95, -90, '$PRISMS_u$', fontsize=20)
-plt.text(185, -90, '$PRISMS_o$', fontsize=20)
-plt.text(260, -90, '$AFTER_u$', fontsize=20)
-plt.text(300, -90, '$AFTER_o$', fontsize=20)
-plt.title(f'SUBJECT {subject + 1} {file}', fontsize=22)
+plt.text(105, -90, '$PRISMS$', fontsize=20)
+plt.text(170, -90, '$AFTER_u$', fontsize=20)
+plt.text(210, -90, '$AFTER_o$', fontsize=20)
+plt.title(f'SUBJECT {subject + 1}', fontsize=22)
 
 # PC standard deviation of the 8 throws before googles
 PC_1 = np.std(exp_data[subject][stage[0]])
 PC_2 = np.std(exp_data[subject][stage[1]])
 plt.text(-5, 90, f'$PC = {PC_1:.1f}$', fontsize=20)
 plt.text(37, 90, f'$PC = {PC_2:.1f}$', fontsize=20)
-plt.text(95, 90, f'$AC = {model[stage[2]][2]:.1f}$', fontsize=20)
-plt.text(185, 90, f'$AC = {model[stage[3]][2]:.1f}$', fontsize=20)
-plt.text(260, 90, f'$AC = {model[stage[4]][2]:.1f}$', fontsize=20)
-plt.text(300, 90, f'$AC = {model[stage[5]][2]:.1f}$', fontsize=20)
-plt.savefig('media/final_plots/exp_3/5.png')
-output=f"""
-PCu = {PC_1:.1f}
-PCo = {PC_1:.1f}
-ACpu = {model[stage[2]][2]:.1f}
-ACpo = {model[stage[3]][2]:.1f}
-ACu =  {model[stage[4]][2]:.1f}
-ACo =  {model[stage[5]][2]:.1f}
-"""
-print(output)
+plt.text(110, 90, f'$AC = {model[stage[2]][2]:.1f}$', fontsize=20)
+plt.text(170, 90, f'$AC = {model[stage[3]][2]:.1f}$', fontsize=20)
+plt.text(210, 90, f'$AC = {model[stage[4]][2]:.1f}$', fontsize=20)
 plt.show()
-print(model)
 
-#plt.savefig('media/final_plots/avg.png')
+print(f"PCu = {PC_1:.1f}")
+print(f"PCo = {PC_2:.1f}")
+print(f"ACpo = {model[stage[2]][2]:.1f}")
+print(f"ACu = {model[stage[3]][2]:.1f}")
+print(f"ACo = {model[stage[4]][2]:.1f}")
+plt.savefig('media/final_plots/exp_2/1.png')
 
-# Exp2
-# plt.xlabel(xlabel, fontsize=22)
-# plt.yticks(fontsize=20)
-# plt.xticks([])
-# plt.text(-5, -90, '$BEFORE_u$', fontsize=20)
-# plt.text(37, -90, '$BEFORE_o$', fontsize=20)
-# plt.text(105, -90, '$PRISMS$', fontsize=20)
-# plt.text(170, -90, '$AFTER_u$', fontsize=20)
-# plt.text(210, -90, '$AFTER_o$', fontsize=20)
-# plt.title(f'SUBJECT {subject + 1}', fontsize=22)
-#
-# # PC standard deviation of the 8 throws before googles
-# PC_1 = np.std(exp_data[subject][stage[0]])
-# PC_2 = np.std(exp_data[subject][stage[1]])
-# plt.text(-5, 90, f'$PC = {PC_1:.1f}$', fontsize=20)
-# plt.text(37, 90, f'$PC = {PC_2:.1f}$', fontsize=20)
-# plt.text(110, 90, f'$AC = {model[stage[2]][2]:.1f}$', fontsize=20)
-# plt.text(170, 90, f'$AC = {model[stage[3]][2]:.1f}$', fontsize=20)
-# plt.text(210, 90, f'$AC = {model[stage[4]][2]:.1f}$', fontsize=20)
-# plt.show()
